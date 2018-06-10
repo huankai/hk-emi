df2912b7a5086eb23b5c4b2b07420e0814f24dbd
huangkai <huankai@139.com>
2018-06-10 10:28:44 +0800

    commit

---
+++ hk-emi-core/src/main/java/com/hk/emi/core/mapper/BaseCodeMapper.java
@@ -0,0 +1,14 @@
+package com.hk.emi.core.mapper;
+
+import com.hk.core.data.mybatis.mapper.BaseMapper;
+import com.hk.emi.core.domain.BaseCode;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author huangkai
+ * @date 2018-6-9 9:30
+ */
+@Mapper
+public interface BaseCodeMapper extends BaseMapper<BaseCode, BaseCode> {
+
+}
